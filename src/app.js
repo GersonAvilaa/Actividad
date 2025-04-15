56873dcd218e4363310a0a538e2071a1a5c3a236
GersonAvilaa <gelian.avila17@gmail.com>
2025-04-14 19:49:22 -0500

    HTTP-GET

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from "express";
 import categoriasRoutes from "./routes/categorias.routes.js"
-
+import cors from "cors"
 
 const app = express();
 
 
 
 app.set("port",5000);
-
+app.use(cors());
 
 /* routes */
 app.use("/api/categorias",categoriasRoutes);
